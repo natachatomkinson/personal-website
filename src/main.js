d8681d333a69401e0e84ddbf117d787bfa2dc5ff
Natacha Segala <natseg@gmail.com>
2020-06-13 14:02:56 +0100

    Change name and content – first iteration

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import App from "./App.svelte";
+
+const app = new App({
+  target: document.body,
+  props: {
+    name: "Natacha Tomkinson",
+    languages: ["Javascript", "Node", "Ruby", "Elixir"],
+    frameworks: ["React", "Rails", "Phoenix"],
+    tools: ["intellij", "vscode", "github", "docker", "docker-compose", "aws"],
+    interests: ["C", "Svelte"],
+  },
+});
+
+export default app;
