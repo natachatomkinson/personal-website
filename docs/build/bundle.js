03c4154ff6896eda6ec69b1bcd8b8658f18bad90
Natacha Tomkinson <natacha.tomkinson@york.ac.uk>
2020-09-25 08:43:12 +0100

    Add build

--- docs/build/bundle.js
+++ docs/build/bundle.js
@@ -24,14 +24,14 @@ var app = (function () {
   function i(t) {
     t.parentNode.removeChild(t);
   }
-  function c(t) {
+  function l(t) {
     return document.createElement(t);
   }
-  function l(t) {
+  function c(t) {
     return document.createTextNode(t);
   }
   function u() {
-    return l(" ");
+    return c(" ");
   }
   function f(t, e, n) {
     null == n
@@ -39,40 +39,40 @@ var app = (function () {
       : t.getAttribute(e) !== n && t.setAttribute(e, n);
   }
   function d(t, e) {
-    (e = "" + e), t.data !== e && (t.data = e);
+    (e = "" + e), t.wholeText !== e && (t.data = e);
   }
-  let m;
-  function p(t) {
-    m = t;
+  let p;
+  function m(t) {
+    p = t;
   }
-  const g = [],
-    h = [],
-    y = [],
+  const h = [],
+    g = [],
     $ = [],
+    y = [],
     b = Promise.resolve();
-  let v = !1;
-  function x(t) {
-    y.push(t);
+  let k = !1;
+  function v(t) {
+    $.push(t);
   }
-  let w = !1;
-  const k = new Set();
-  function _() {
-    if (!w) {
-      w = !0;
+  let _ = !1;
+  const x = new Set();
+  function w() {
+    if (!_) {
+      _ = !0;
       do {
-        for (let t = 0; t < g.length; t += 1) {
-          const e = g[t];
-          p(e), j(e.$$);
+        for (let t = 0; t < h.length; t += 1) {
+          const e = h[t];
+          m(e), j(e.$$);
         }
-        for (g.length = 0; h.length; ) h.pop()();
-        for (let t = 0; t < y.length; t += 1) {
-          const e = y[t];
-          k.has(e) || (k.add(e), e());
+        for (m(null), h.length = 0; g.length; ) g.pop()();
+        for (let t = 0; t < $.length; t += 1) {
+          const e = $[t];
+          x.has(e) || (x.add(e), e());
         }
-        y.length = 0;
-      } while (g.length);
-      for (; $.length; ) $.pop()();
-      (v = !1), (w = !1), k.clear();
+        $.length = 0;
+      } while (h.length);
+      for (; y.length; ) y.pop()();
+      (k = !1), (_ = !1), x.clear();
     }
   }
   function j(t) {
@@ -81,20 +81,20 @@ var app = (function () {
       const e = t.dirty;
       (t.dirty = [-1]),
         t.fragment && t.fragment.p(t.ctx, e),
-        t.after_update.forEach(x);
+        t.after_update.forEach(v);
     }
   }
   const z = new Set();
   function E(t, e) {
     -1 === t.$$.dirty[0] &&
-      (g.push(t), v || ((v = !0), b.then(_)), t.$$.dirty.fill(0)),
+      (h.push(t), k || ((k = !0), b.then(w)), t.$$.dirty.fill(0)),
       (t.$$.dirty[(e / 31) | 0] |= 1 << e % 31);
   }
-  function C(s, a, c, l, u, f, d = [-1]) {
-    const g = m;
-    p(s);
-    const h = a.props || {},
-      y = (s.$$ = {
+  function C(s, a, l, c, u, f, d = [-1]) {
+    const h = p;
+    m(s);
+    const g = a.props || {},
+      $ = (s.$$ = {
         fragment: null,
         ctx: null,
         props: f,
@@ -105,126 +105,127 @@ var app = (function () {
         on_destroy: [],
         before_update: [],
         after_update: [],
-        context: new Map(g ? g.$$.context : []),
+        context: new Map(h ? h.$$.context : []),
         callbacks: n(),
         dirty: d,
+        skip_bound: !1,
       });
-    let $ = !1;
+    let y = !1;
     if (
-      ((y.ctx = c
-        ? c(s, h, (t, e, ...n) => {
+      (($.ctx = l
+        ? l(s, g, (t, e, ...n) => {
             const o = n.length ? n[0] : e;
             return (
-              y.ctx &&
-                u(y.ctx[t], (y.ctx[t] = o)) &&
-                (y.bound[t] && y.bound[t](o), $ && E(s, t)),
+              $.ctx &&
+                u($.ctx[t], ($.ctx[t] = o)) &&
+                (!$.skip_bound && $.bound[t] && $.bound[t](o), y && E(s, t)),
               e
             );
           })
         : []),
-      y.update(),
-      ($ = !0),
-      o(y.before_update),
-      (y.fragment = !!l && l(y.ctx)),
+      $.update(),
+      (y = !0),
+      o($.before_update),
+      ($.fragment = !!c && c($.ctx)),
       a.target)
     ) {
       if (a.hydrate) {
         const t = (function (t) {
           return Array.from(t.childNodes);
         })(a.target);
-        y.fragment && y.fragment.l(t), t.forEach(i);
-      } else y.fragment && y.fragment.c();
-      a.intro && (b = s.$$.fragment) && b.i && (z.delete(b), b.i(v)),
+        $.fragment && $.fragment.l(t), t.forEach(i);
+      } else $.fragment && $.fragment.c();
+      a.intro && (b = s.$$.fragment) && b.i && (z.delete(b), b.i(k)),
         (function (t, n, s) {
           const {
             fragment: a,
             on_mount: i,
-            on_destroy: c,
-            after_update: l,
+            on_destroy: l,
+            after_update: c,
           } = t.$$;
           a && a.m(n, s),
-            x(() => {
+            v(() => {
               const n = i.map(e).filter(r);
-              c ? c.push(...n) : o(n), (t.$$.on_mount = []);
+              l ? l.push(...n) : o(n), (t.$$.on_mount = []);
             }),
-            l.forEach(x);
+            c.forEach(v);
         })(s, a.target, a.anchor),
-        _();
+        w();
     }
-    var b, v;
-    p(g);
+    var b, k;
+    m(h);
   }
   function N(e) {
     let n,
       o,
       r,
       s,
-      m,
       p,
-      g,
+      m,
       h,
-      y,
+      g,
       $,
+      y,
       b,
+      k,
       v,
+      _,
       x,
       w,
-      k,
-      _,
       j,
       z,
       E,
       C,
       N,
       A,
+      O,
       R,
       S,
-      I,
-      O,
-      P = e[3].join(", ") + "",
-      T = e[1].join(", ") + "",
+      T,
+      I = e[3].join(", ") + "",
+      P = e[1].join(", ") + "",
       q = e[2].join(", ") + "",
       B = e[4].join(", ") + "";
     return {
       c() {
-        (n = c("main")),
-          (o = c("h1")),
-          (r = l("Hello, I am ")),
-          (s = l(e[0])),
-          (m = l("!")),
-          (p = u()),
-          (g = c("div")),
-          (h = c("p")),
-          (h.textContent = "Well, I have been busy"),
-          (y = u()),
-          ($ = c("p")),
-          (b = l("writing production apps in these languages: ")),
-          (v = l(P)),
-          (x = u()),
-          (w = c("p")),
-          (k = l("using these frameworks: ")),
-          (_ = l(T)),
+        (n = l("main")),
+          (o = l("h1")),
+          (r = c("Hello, I am ")),
+          (s = c(e[0])),
+          (p = c("!")),
+          (m = u()),
+          (h = l("div")),
+          (g = l("p")),
+          (g.textContent = "Well, I have been busy"),
+          ($ = u()),
+          (y = l("p")),
+          (b = c("writing production apps in these languages: ")),
+          (k = c(I)),
+          (v = u()),
+          (_ = l("p")),
+          (x = c("using these frameworks: ")),
+          (w = c(P)),
           (j = u()),
-          (z = c("p")),
-          (E = l("with some of these really cool tools: ")),
-          (C = l(q)),
+          (z = l("p")),
+          (E = c("with some of these really cool tools: ")),
+          (C = c(q)),
           (N = u()),
-          (A = c("p")),
-          (R = l("while learning more about: ")),
-          (S = l(B)),
-          (I = u()),
-          (O = c("a")),
-          (O.textContent = "Email me"),
+          (A = l("p")),
+          (O = c("while learning more about: ")),
+          (R = c(B)),
+          (S = u()),
+          (T = l("a")),
+          (T.textContent = "Email me"),
           f(o, "class", "svelte-zy6d0d"),
-          f(h, "class", "svelte-zy6d0d"),
-          f($, "class", "svelte-zy6d0d"),
-          f(w, "class", "svelte-zy6d0d"),
+          f(g, "class", "svelte-zy6d0d"),
+          f(y, "class", "svelte-zy6d0d"),
+          f(_, "class", "svelte-zy6d0d"),
           f(z, "class", "svelte-zy6d0d"),
           f(A, "class", "svelte-zy6d0d"),
-          f(g, "class", "home-main-description svelte-zy6d0d"),
-          f(O, "href", "mailto:website@natachatomkinson.co.uk"),
-          f(O, "target", "_blank"),
-          f(O, "rel", "noopener noreferrer"),
+          f(h, "class", "home-main-description svelte-zy6d0d"),
+          f(T, "href", "mailto:website@natachatomkinson.co.uk"),
+          f(T, "target", "_blank"),
+          f(T, "rel", "noopener noreferrer"),
           f(n, "class", "svelte-zy6d0d");
       },
       m(t, e) {
@@ -234,35 +235,35 @@ var app = (function () {
           a(n, o),
           a(o, r),
           a(o, s),
-          a(o, m),
-          a(n, p),
-          a(n, g),
-          a(g, h),
-          a(g, y),
-          a(g, $),
-          a($, b),
-          a($, v),
-          a(g, x),
-          a(g, w),
-          a(w, k),
-          a(w, _),
-          a(g, j),
-          a(g, z),
+          a(o, p),
+          a(n, m),
+          a(n, h),
+          a(h, g),
+          a(h, $),
+          a(h, y),
+          a(y, b),
+          a(y, k),
+          a(h, v),
+          a(h, _),
+          a(_, x),
+          a(_, w),
+          a(h, j),
+          a(h, z),
           a(z, E),
           a(z, C),
-          a(g, N),
-          a(g, A),
+          a(h, N),
+          a(h, A),
+          a(A, O),
           a(A, R),
-          a(A, S),
-          a(n, I),
-          a(n, O);
+          a(n, S),
+          a(n, T);
       },
       p(t, [e]) {
         1 & e && d(s, t[0]),
-          8 & e && P !== (P = t[3].join(", ") + "") && d(v, P),
-          2 & e && T !== (T = t[1].join(", ") + "") && d(_, T),
+          8 & e && I !== (I = t[3].join(", ") + "") && d(k, I),
+          2 & e && P !== (P = t[1].join(", ") + "") && d(w, P),
           4 & e && q !== (q = t[2].join(", ") + "") && d(C, q),
-          16 & e && B !== (B = t[4].join(", ") + "") && d(S, B);
+          16 & e && B !== (B = t[4].join(", ") + "") && d(R, B);
       },
       i: t,
       o: t,
@@ -278,7 +279,7 @@ var app = (function () {
       { languages: a } = e,
       { interests: i } = e;
     return (
-      (t.$set = (t) => {
+      (t.$$set = (t) => {
         "name" in t && n(0, (o = t.name)),
           "frameworks" in t && n(1, (r = t.frameworks)),
           "tools" in t && n(2, (s = t.tools)),
@@ -310,7 +311,12 @@ var app = (function () {
         }
       );
     }
-    $set() {}
+    $set(t) {
+      var e;
+      this.$$set &&
+        ((e = t), 0 !== Object.keys(e).length) &&
+        ((this.$$.skip_bound = !0), this.$$set(t), (this.$$.skip_bound = !1));
+    }
   } {
     constructor(t) {
       super(),
